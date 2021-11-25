08d74ed2e6a0a2ec0bc2f940ca16157ebd071ed1
Paloma Regis Ferreira <palomaregis29@gmail.com>
2021-11-25 14:24:23 -0300

    Implemetacao dos testes

--- src/test/java/com/digitalinnovationone/heroesapi/HeroesapiApplicationTests.java
+++ src/test/java/com/digitalinnovationone/heroesapi/HeroesapiApplicationTests.java
@@ -1,13 +1,57 @@
 package com.digitalinnovationone.heroesapi;
 
+import com.digitalinnovationone.heroesapi.repository.HeroesRepository;
+//import jdk.internal.misc.JavaIOFileDescriptorAccess;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.awt.*;
+
+import static com.digitalinnovationone.heroesapi.constants.HeroesConstant.HEROES_ENDPOINT_LOCAL;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
+@DirtiesContext
+@AutoConfigureWebTestClient
 class HeroesapiApplicationTests {
 
+	@Autowired
+	WebTestClient webTestClient;
+
+	@Autowired
+	HeroesRepository heroesRepository;
+
 	@Test
-	void contextLoads() {
+	public void getOneHeroById(){
+		webTestClient.get().uri(HEROES_ENDPOINT_LOCAL.concat("/{id}"), "99")
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody();
 	}
+
+	@Test
+	public void getOneHeroNotFound(){
+		webTestClient.get().uri(HEROES_ENDPOINT_LOCAL.concat("/{id}"), "18")
+				.exchange()
+				.expectStatus().isNotFound();
+	}
+
+	@Test
+	public void deleteHero(){
+		webTestClient.delete().uri(HEROES_ENDPOINT_LOCAL.concat("/{id}"), "1")
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isNotFound()
+				.expectBody(Void.class);
+	}
+
+
 
 }
