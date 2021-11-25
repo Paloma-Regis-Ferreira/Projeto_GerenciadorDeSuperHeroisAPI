e417f687c3055fae1d814ed80e694a8308d4e336
Paloma-Regis-Ferreira <palomaregis29@gmail.com>
2021-11-25 00:45:59 -0200

    iniciando o projeto e configuracao

---
+++ src/main/java/com/digitalinnovationone/heroesapi/config/DynamoConfig.java
@@ -0,0 +1,52 @@
+package com.digitalinnovationone.heroesapi.config;
+
+import com.amazonaws.AmazonWebServiceRequest;
+import com.amazonaws.ResponseMetadata;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Region;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.model.*;
+import com.amazonaws.services.dynamodbv2.waiters.AmazonDynamoDBWaiters;
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.SpringUtils;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+import java.util.Map;
+
+@Configuration
+@EnableDynamoDBRepositories
+public class DynamoConfig {
+    @Value("${amazon.dynamodb.endpoint}")
+    private String amazonDynamoDBEndpoint;
+
+    @Value("${aws_access_key_id}")
+    private String amazonAWSAcessKey;
+
+    @Value("${aws_secret_access_key}")
+    private String amazonAWSSecretKey;
+
+    @Bean
+    public AmazonDynamoDB amazonDynamoDB() {
+        AmazonDynamoDB amazonDynamoDB = new AmazonDynamoDBClient(amazonAWSCredentials());
+            if(!StringUtils.isEmpty(amazonDynamoDBEndpoint)){
+                amazonDynamoDB.setEndpoint(amazonDynamoDBEndpoint);
+            }
+            return amazonDynamoDB;
+    }
+
+    @Bean
+    public AWSCredentials amazonAWSCredentials() {
+        return new BasicAWSCredentials(
+                amazonAWSAcessKey,amazonAWSSecretKey;
+        )
+    }
+}
+
+
+
